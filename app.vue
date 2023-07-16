4b0dba3557e14c1b5bc47937a7e0a8a41956b18c
AbdelrhmanAbuelnile <abdohani362@gmail.com>
2023-07-17 00:00:14 +0300

    first section

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-full bg-body">
-    <Header />
+  <div class="h-screen bg-body md:p-10 p-4 flex flex-col justify-center items-center gap-4">
+    <Header class="md:rounded-[32px] rounded-2xl w-full h-screen" />
   </div>
 </template>
--- components/Header.vue
+++ components/Header.vue
@@ -1,31 +1,39 @@
 <template>
-    <div class="hello text-primary-white flex flex-col flex-1 py-5 h-screen">
-        <header class="flex flex-row justify-between items-center border-b border-header-border w-11/12 mx-auto">
-            <div class="flex flex-row gap-4 justify-between items-center pb-5">
+    <div class="hello text-primary-white flex flex-col flex-1 md:py-5 py-3 h-screen overflow-x-hidden">
+        <header class="flex flex-row justify-between items-center lg:gap-64 border-b border-header-border md:py-16 lg:w-11/12 px-3 mx-auto">
+            <div class="flex flex-row md:gap-4 gap-2 justify-between items-center pb-5">
                 <img src="../assets/imgs/logotype.svg" alt="logo">
-                <a href="#" class="bg-card-red text-base font-semibold py-[15px] px-4 rounded-lg flex flex-row justify-between items-center gap-2">
-                    <Icon name="uil:github" />
+                <button @click="show = !show" class="ml-14 bg-card-red text-base font-semibold py-[15px] px-4 rounded-lg flex flex-row justify-between items-center gap-2">
+                    <Icon name="ic:sharp-menu" class="text-xl"/>
                     <p>Services</p>
-                </a>
+                </button>
+                <div v-if="show" class="absolute top-24 lg:top-0 right-0 lg:left-0 py-10 lg:py-16 lg:w-1/6 w-full h-screen bg-body">
+                    <ul class="flex flex-col justify-center items-start px-4 gap-8">
+                        <li><a href="#" class="text-4xl border-b border-slate-300 hover:text-slate-300 duration-150">Works</a></li>
+                        <li><a href="#" class="text-4xl border-b border-slate-300 hover:text-slate-300 duration-150">About</a></li>
+                        <li><a href="#" class="text-4xl border-b border-slate-300 hover:text-slate-300 duration-150">Stoks</a></li>
+                        <li><a href="#" class="text-4xl border-b border-slate-300 hover:text-slate-300 duration-150">Contacts</a></li>
+                    </ul>
+                </div>
             </div>
-            <nav>
-                <ul class="flex flex-row justify-center items-center gap-16 text-base font-medium">
+            <nav class="ml-80 hidden md:block">
+                <ul class="flex flex-row justify-center items-center gap-14 text-base font-medium">
                     <li><a href="#" class="hover:text-slate-300 duration-150">Works</a></li>
                     <li><a href="#" class="hover:text-slate-300 duration-150">About</a></li>
                     <li><a href="#" class="hover:text-slate-300 duration-150">Stoks</a></li>
                     <li><a href="#" class="hover:text-slate-300 duration-150">Contacts</a></li>
                 </ul>
             </nav>
-            <a href="#" class="hover:text-slate-300 duration-150">+0123456789</a>
+            <a href="#" class="hover:text-slate-300 duration-150 hidden md:block">+0123456789</a>
         </header>
-        <div class="flex flex-col items-start justify-start gap-10 px-20 mt-64">
-            <p class="text-[64px] font-medium">Premium service <br> for your car</p>
-            <div class="flex flex-row gap-4 justify-center items-center text-lg font-semibold">
-                <button class="rounded-2xl py-5 px-10 bg-card-red flex flex-row gap-4 justify-center items-center">
+        <div class="flex flex-col items-start justify-start gap-10 lg:px-20 px-3 mt-56">
+            <p class="lg:text-[64px] text-4xl font-medium">Premium service <br> for your car</p>
+            <div class="flex flex-col md:flex-row gap-4 justify-center items-center text-base md:text-lg font-semibold">
+                <button class="w-full rounded-2xl py-6 pr-10 pl-12 bg-card-red flex flex-row gap-1 justify-center items-center">
                     <p>Registration</p>
                 </button>
-                <button class="rounded-2xl py-5 px-10 border border-primary-white flex flex-row gap-4 justify-center items-center">
-                    <Icon name="uil:github" />
+                <button class="rounded-2xl py-6 pr-10 pl-12 border border-primary-white flex flex-row gap-1 justify-center items-center">
+                    <Icon name="ph:chat-circle-text-thin" class="text-3xl"/>
                     <p>Consultation</p>
                 </button>
             </div>
@@ -34,7 +42,8 @@
 </template>
 
 <script setup>
-
+import {ref} from 'vue'
+let show = ref(false)
 </script>
 
 <style scoped>
